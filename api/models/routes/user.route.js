55fdb806c1ddb40fa5756a24a548c6adb55c4500
gksusha <gauravkumarsingh885300@gmail.com>
2025-11-02 21:47:00 +0530

    create a test api route

---
+++ api/models/routes/user.route.js
@@ -0,0 +1,10 @@
+import e from 'express';
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.json({ message: 'Api is working' });
+}
+);
+export default router;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,53 +2,59 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom' 
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import {Navbar, NavbarCollapse, NavbarLink,TextInput,NavbarToggle} from 'flowbite-react'
 
 export default function Header() {
   const path = useLocation().pathname;
   return (
-    <nav className='border-b-2'>
+    <Navbar fluid rounded>
 
       <Link to ="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
       <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sahand's</span>
       </Link>
       <form>
-        <textinput type='text'
+        <TextInput type='text'
         placpeholder='Search...' 
         righticon={<AiOutlineSearch/>}
         className='hidden lg:inline'
         />
       </form>
-      <button className='w-12 h10 lg:hidden' color='gray' pill>
+      <button className='w-12 h10 lg:hidden' color='gray'>
         <AiOutlineSearch />
       </button>
       <div className='flex gap-2 md:order-2'>
-        <button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+        <button className='w-12 h-10 hidden sm:inline' color='gray'>
           <FaMoon />
         </button>
         <Link to='/signin'>
-        <button gradientDuoTone='purpleToBlue' outline>
+        <button>
         Sign In
         </button>
         </Link>
-        <navbar.toggle />
-        </div>
-        <navbar.collapse>
-          <navbar.link active={path === '/'} as={div}>
-          <Link to='/'>Home</Link> 
-          </navbar.link >
-          <navbar.link active={path === "/about"} as={div}>
-          <Link to='/about'>
-            About
-          </Link >
-          </navbar.link> 
-          <navbar.link active={path === "/projects"} as={div}>
-            <Link to='projects'>
-             Projects
-            </Link>
-            
-          </navbar.link>
-        </navbar.collapse>
-    </nav>
+      </div>
+      <NavbarToggle />
+        <NavbarCollapse>
+  <NavbarLink as={Link} to="/" active={path === "/"}>
+    Home
+  </NavbarLink>
+
+  <NavbarLink as={Link} to="/about" active={path === "/about"}>
+    About
+  </NavbarLink>
+
+  <NavbarLink as={Link} to="/projects" active={path === "/projects"}>
+    Projects
+  </NavbarLink>
+  <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#">About</NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+</NavbarCollapse>
+
+    </Navbar>
 
   )
 
