887a2c9c5f93b8dfd999a417ef0514ffdf044897
gksusha <gauravkumarsingh885300@gmail.com>
2025-11-26 04:51:39 +0530

    add functionality to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,45 @@
-import { Label, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
-import { Button, Spinner } from 'flowbite-react';
-
+import { Alert, Button, Label, TextInput, Spinner } from 'flowbite-react';
+import { useState} from 'react';
+import { Link, useNavigate  } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errormessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+  setFormData({ ...formData, [e.target.id]: e.target.value.trim()
+  });
+  };
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  if (!formData.username || !formData.email || !formData.password) {
+    return setErrorMessage('All fields are required');
+  }
+  try {
+    setLoading(true);
+    setErrorMessage(null);
+    const res = await fetch('/api/auth/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    if (data.success === false) {
+        return setErrorMessage(data.message);
+    }
+    setLoading(false);
+     if (res.ok) {
+        navigate('/sign-in');
+      }
+  } catch (error) {
+    setErrorMessage(error.message);
+    setLoading(false);
+  }
+};
+  
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -22,39 +58,47 @@ export default function SignUp() {
         </div>
         {/*right side*/}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
-            <div className=''>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
-
+                onChange={handleChange}
               />
             </div>
-             <div>
+            <div>
               <Label value='Your email' />
               <TextInput
                 type='email'
                 placeholder='name@company.com'
                 id='email'
-                
+                onChange={handleChange}
               />
             </div>
-             <div>
+            <div>
               <Label value='Your password' />
               <TextInput
                 type='password'
                 placeholder='Password'
                 id='password'
-                
+                onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone='purpleToPink'
               type='submit'
-             
-            >Sign Up
+              className="text-white bg-gradient-to-r from-purple-500 to-pink-500"
+              disabled={loading}>
+                {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
+
             </Button>
 
           </form>
@@ -64,6 +108,14 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {
+            errormessage && (
+              <Alert className='mt-5' color='failure'
+              >
+                {errormessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,14 @@ import tailwindcss from '@tailwindcss/vite'   // Tailwind plugin
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target:'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [// React plugin
   react(), // Tailwind plugin
   tailwindcss(), flowbiteReact()],
