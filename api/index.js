6f544eef4f1ecb58432c3cceb705999bc234196d
gksusha <gauravkumarsingh885300@gmail.com>
2025-11-25 23:52:27 +0530

    create

--- api/index.js
+++ api/index.js
@@ -5,14 +5,17 @@ import userRoutes from './models/routes/user.route.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
   console.log('Mongodb is connected');
 })
 .catch((err) => {
   console.log(err);
 });
+
 const app = express(); 
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });  
--- api/models/routes/user.route.js
+++ api/models/routes/user.route.js
@@ -1,10 +1,9 @@
 import e from 'express';
 import express from 'express';
+import { test } from '../../controllers/user.controller.js'; 
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.json({ message: 'Api is working' });
-}
+router.get('/test', test 
 );
-export default router;
+export default router; 
